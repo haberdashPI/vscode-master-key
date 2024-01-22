b0dfddcb5d7751a11cb0bb467f242845efb38b22
David Little <david.frank.little@gmail.com>
2024-01-21 22:11:40 -0500

    some initial search tests

--- src/web/test/uxsuite/searchMotions.ts
+++ src/web/test/uxsuite/searchMotions.ts
@@ -1,10 +1,12 @@
 import { pause, movesCursorInEditor, setBindings, setupEditor } from "./utils";
 import expect from "expect";
-import { Key, TextEditor, Workbench } from "vscode-extension-tester";
+import { InputBox, Key, TextEditor, Workbench } from "vscode-extension-tester";
 
 export const run = () => describe('Search motions', () => {
+    let editor: TextEditor;
+
     before(async function(){
-        this.timeout(5 * 1000);
+        this.timeout(8 * 1000);
         await setBindings(`
             [header]
             version = "1.0"
@@ -17,20 +19,67 @@ export const run = () => describe('Search motions', () => {
             key = "escape"
             mode = []
             command = "runCommands"
-            args = ["master-key.enterInsert", "master-key.reset"]
-            when = "!findWidgetVisible"
+            args = ["master-key.enterNormal", "master-key.reset"]
             prefixes = "<all-prefixes>"
+
+            [[path]]
+            name = "search"
+            id = "search"
+            default.mode = "normal"
+            default.command = "master-key.search"
 
             [[bind]]
             name = "search"
             key = "/"
-            command = "master-key.search"
+            path = "search"
+
+            [[bind]]
+            name = "searchC"
+            key = "shift+/"
+            path = "search"
+            args.backwards = true
        `);
+
+       editor = await setupEditor(`foobar bum POINT_A Officia voluptate ex commodo esse laborum velit
+ipsum velit excepteur sunt cillum nulla adipisicing cupidatat. Laborum officia do mollit do
+labore elit occaecat cupidatat non POINT_B.`);
+    });
+
+    it.only('Handles basic search', async () => {
+        await editor.moveCursor(1, 1);
+        await editor.typeText(Key.ESCAPE);
+        await pause(250);
+
+        await movesCursorInEditor(async () => {
+            await editor.typeText('/');
+            await pause(50);
+            const input = await InputBox.create();
+
+            await input.setText('POINT_A');
+            await input.confirm();
+            await pause(100);
+        }, [0, 10], editor);
+    });
+
+    it.only('Handles backwards search', async () => {
+        await editor.moveCursor(2, 1);
+        await editor.typeText(Key.ESCAPE);
+        await pause(250);
+
+        await movesCursorInEditor(async () => {
+            await editor.typeText(Key.chord(Key.SHIFT,'/'));
+            await pause(50);
+            const input = await InputBox.create();
+
+            await input.setText('POINT_A');
+            await input.confirm();
+            await pause(100);
+        }, [0, 18], editor);
     });
 
     // TODO: start working on testing out the most basic command
     // TODO: test out each argument
-    // - backwards
+    // + backwards
     // - caseSensitive
     // - wrapAround
     // - acceptAfter
@@ -41,7 +90,10 @@ export const run = () => describe('Search motions', () => {
     // - regex
     // - register
     // - doAfter
-
+    // TODO: test correctness of cancelling out a search
+    // TODO: test correctness of failing to find search in text
+    // TODO: test out 'delete last char'
+    // TODO: test out next and previous match
 });
 
 export default { run };
