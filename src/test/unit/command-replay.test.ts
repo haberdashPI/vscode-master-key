71fbae651eb74cc57990b86c7bc624acb475c8f7
David Little <david.frank.little@gmail.com>
2025-07-03 00:22:01 -0400

    try running ci on macos
    Created using spr 1.3.6-beta.1

---
+++ src/test/unit/command-replay.test.ts
@@ -0,0 +1,50 @@
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+import * as assert from 'assert';
+import { assertCursorMovesBy, cursorToPos, cursorToStart, editorWithText } from './utils';
+
+suite('Search command', () => {
+    let editor: vscode.TextEditor;
+    setup(async () => {
+        editor = await editorWithText(`a b c d
+e f g h
+i j k l`);
+    });
+
+    test('Handles recording', async () => {
+        cursorToStart(editor);
+        await assertCursorMovesBy(editor, { line: 1, character: 1 }, async () => {
+            await vscode.commands.executeCommand('master-key.do', {
+                do: [{ command: 'master-key.record', args: { on: true } }],
+            });
+            await vscode.commands.executeCommand('master-key.do', {
+                do: [{ command: 'cursorMove', args: { to: 'right' } }],
+            });
+            await vscode.commands.executeCommand('master-key.do', {
+                do: [{ command: 'cursorMove', args: { to: 'down' } }],
+            });
+            await vscode.commands.executeCommand('master-key.do', {
+                do: [
+                    {
+                        command: 'master-key.record',
+                        args: { on: false },
+                    },
+                    {
+                        command: ' master-key.pushHistoryToStack',
+                        args: {
+                            whereComputedRangeIs: {
+                                from: 'commandHistory[index-1].name === "record"',
+                                to: 'index',
+                            },
+                        },
+                    },
+                ],
+            });
+        });
+
+        await assertCursorMovesBy(editor, { line: 1, character: 1 }, async () => {
+            await vscode.commands.executeCommand('master-key.replayFromStack');
+        });
+    });
+});
