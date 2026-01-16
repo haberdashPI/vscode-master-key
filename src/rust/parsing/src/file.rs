2f6c4673b41293a0c32aafdd422030c7b342bd46
David Little <david.frank.little@gmail.com>
2026-01-16 12:00:37 -0500

    Feature: implement `before/after` for `[[define.bind]]`
    `before/after` prepend/append a sequence of commands to the main `command`;
    improves the ergonomics of implementing operators for Vim bindings.
    
    Pull Request: https://github.com/haberdashPI/vscode-master-key/pull/148

--- src/rust/parsing/src/file.rs
+++ src/rust/parsing/src/file.rs
@@ -4,8 +4,8 @@
 ///
 /// # Master Keybindings
 ///
-/// This defines version 2.0 of the master keybinding file format. Breaking changes from
-/// version 1.0 are [described below](#breaking-changes)
+/// This defines version 2.1 of the master keybinding file format. All changes (including
+/// breaking) are [described below](#breaking-changes)
 ///
 /// Master keybindings are [TOML](https://toml.io/en/) files that begin with a line
 /// containing `#:master-keybindings` and include the following top-level
@@ -101,6 +101,16 @@
 /// ```
 /// ## Breaking Changes
 ///
+/// ### 2.1
+///
+/// The following, non-breaking changes were introduced in this version
+///
+/// - `define.bind.before/after`: Default binding definitions can now include a sequence of
+/// commands to execute before or after the command or commands executed with
+/// `bind.command`.
+///
+/// ### 2.0
+///
 /// The following changes were made from version 1.0 of the file format.
 ///
 /// - `header.version` is now 2.0
@@ -133,6 +143,11 @@
 ///   - removed `(re)storeNamed` commands
 ///   - replay-related command fields have changed their semantics, see examples
 ///     under [replayFromHistory](/commands/replayFromHistory)
+///
+/// ### 1.0
+///
+/// THis was the original file format version
+///
 #[allow(unused_imports)]
 use log::{error, info};
 
@@ -255,7 +270,7 @@ impl KeyFile {
         let version = input.header.version.as_ref();
         if !VersionReq::parse("2.0").unwrap().matches(version) {
             let r: Result<()> = Err(wrn!(
-                "This version of master key is only compatible with the 2.0 file format."
+                "This version of master key is only compatible version 2 of the file format."
             ))
             .with_range(&input.header.version.span());
             errors.push(r.unwrap_err().into());
@@ -291,7 +306,7 @@ impl KeyFile {
             }
         };
         let mut define = if !skip_define {
-            match Define::new(define_input, &mut scope, warnings) {
+            match Define::new(define_input, &mut scope, warnings, version) {
                 Err(mut es) => {
                     errors.append(&mut es.errors);
                     Define::default()
@@ -329,7 +344,23 @@ impl KeyFile {
 
         // [[bind]]
         let input_iter = input.bind.into_iter().flatten().map(|x| {
+            // validate `before/after`
             let span = x.span().clone();
+            if !x.as_ref().before.is_none() {
+                errors.push(
+                    Result::<()>::Err(err!("`before` is reserved for `[[defined.bind]]`").into())
+                        .with_range(&span)
+                        .unwrap_err(),
+                );
+            }
+            if !x.as_ref().before.is_none() {
+                errors.push(
+                    Result::<()>::Err(err!("`before` is reserved for `[[defined.bind]]`").into())
+                        .with_range(&span)
+                        .unwrap_err(),
+                );
+            }
+
             return Ok(Spanned::new(
                 span.clone(),
                 define.expand(x.into_inner()).with_range(&span)?,
@@ -3337,6 +3368,69 @@ pub(crate) mod tests {
             })
             .collect();
         assert_eq!(prefixes.len(), 1);
+    }
+
+    #[test]
+    fn before_after_commands() {
+        let data = r#"
+        #:master-keybindings
+
+        [header]
+        version = "2.0.0"
+
+        [[define.bind]]
+        id = "add_commands"
+
+        [[define.bind.after]]
+        command = "foo"
+
+        [[define.bind.before]]
+        command = "bar"
+
+        [[bind]]
+        default = "{{bind.add_commands}}"
+        key = "cmd+b"
+        command = "biz"
+        "#;
+        let result = parse_keybinding_data(&data);
+        let commands = result.file.unwrap().bind[0].clone().commands;
+        assert_eq!(commands[0].command, "bar");
+        assert_eq!(commands[1].command, "biz");
+        assert_eq!(commands[2].command, "foo");
+
+        assert!(
+            result.errors.as_ref().unwrap()[0]
+                .message
+                .contains("`before`")
+        );
+        assert!(
+            result.errors.as_ref().unwrap()[1]
+                .message
+                .contains("`after`")
+        );
+
+        let data = r#"
+        #:master-keybindings
+
+        [header]
+        version = "2.1.0"
+
+        [[define.bind]]
+        id = "add_commands"
+
+        [[define.bind.after]]
+        command = "foo"
+
+        [[define.bind.before]]
+        command = "bar"
+
+        [[bind]]
+        default = "{{bind.add_commands}}"
+        key = "cmd+b"
+        command = "biz"
+        "#;
+        let result = parse_keybinding_data(&data);
+        assert_eq!(result.errors.unwrap().len(), 0)
     }
 
     #[test]
