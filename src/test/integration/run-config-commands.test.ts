e743bef46a1ed07854c66b04d54719fc59888019
David Little <david.frank.little@gmail.com>
2026-08-03 11:44:26 -0400

    add integration tests

--- src/test/integration/run-config-commands.test.ts
+++ src/test/integration/run-config-commands.test.ts
@@ -2,13 +2,10 @@ import { test, expect } from './config';
 import { activateKeybindings, openFile, runCommand } from './utils';
 
 test.describe('Configuration Updates', () => {
-    test.beforeEach(async ({ workbox }) => {
+    test('Can add/remove bindings', async ({ workbox }) => {
         await activateKeybindings(workbox, 'simpleMotions.toml');
         await openFile(workbox, 'text.md');
         await workbox.keyboard.press('Escape');
-    });
-
-    test('Can add/remove bindings', async ({ workbox }) => {
         const activateMessage = workbox.getByLabel(
             'Master keybindings were added to \`keybindings.json\`.',
         ).first();
@@ -27,5 +24,29 @@ test.describe('Configuration Updates', () => {
             'div[aria-label="Keybinding Mode: default"]',
         );
         await expect(statusBarMode).toBeAttached();
+    });
+
+    test('Can add and run bindings from `source`', async ({ workbox }) => {
+        await activateKeybindings(workbox, 'vimSource.toml');
+
+        await openFile(workbox, 'macro.md');
+        const editor = workbox.getByLabel('macro.md').
+            filter({ has: workbox.getByText('a b c ') }).
+            filter({ has: workbox.getByRole('code') });
+        const pos = await workbox.getByRole('button').
+            filter({ hasText: /Ln [0-9]+, Col [0-9]+/ });
+        const activateMessage = workbox.getByLabel(
+            'Master keybindings were added to \`keybindings.json\`.',
+        ).first();
+        await expect(activateMessage).toBeAttached();
+
+        await editor.press('l');
+        await expect(pos).toHaveText('Ln 1, Col 2');
+
+        await editor.press('h');
+        await expect(pos).toHaveText('Ln 1, Col 1');
+
+        await editor.press('Shift+k');
+        await expect(pos).toHaveText('Ln 1, Col 2');
     });
 });
